630745df14f92debd70fd9794861d46e59046751
shakhzod <shoh.hashimov.99@gmail.com>
2021-05-28 20:20:09 +0200

    Unit Tests has been added

--- src/test/java/com/shakhzod/blog/BlogApplicationTests.java
+++ src/test/java/com/shakhzod/blog/BlogApplicationTests.java
@@ -10,29 +10,6 @@ class BlogApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	Calculator calculator = new Calculator();
-	@Test
-	void itShouldAddNumbers()
-	{
-		// given
-		int firstNumber = 10;
-		int secondNumber = 20;
-
-		//when
-		int result = calculator.add(firstNumber,secondNumber);
-
-		//then
-		assertThat(result).isEqualTo(30);
-
-	}
-
-	class Calculator
-	{
-		int add(int a, int b)
-		{
-			return a+b;
-		}
-	}
 
 
 }
