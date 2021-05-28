f208510ba60f1be5e78dc1d28ad40720aff72361
shakhzod <shoh.hashimov.99@gmail.com>
2021-05-28 20:16:47 +0200

    Unit Tests has been added

--- src/main/java/com/shakhzod/blog/repo/PostRepository.java
+++ src/main/java/com/shakhzod/blog/repo/PostRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long>
 {
-
+    boolean existsByAnons(String anons);
 
 
 }
--- src/test/java/com/shakhzod/blog/BlogApplicationTests.java
+++ src/test/java/com/shakhzod/blog/BlogApplicationTests.java
@@ -3,11 +3,36 @@ package com.shakhzod.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class BlogApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	Calculator calculator = new Calculator();
+	@Test
+	void itShouldAddNumbers()
+	{
+		// given
+		int firstNumber = 10;
+		int secondNumber = 20;
+
+		//when
+		int result = calculator.add(firstNumber,secondNumber);
+
+		//then
+		assertThat(result).isEqualTo(30);
+
+	}
+
+	class Calculator
+	{
+		int add(int a, int b)
+		{
+			return a+b;
+		}
+	}
+
 
 }
